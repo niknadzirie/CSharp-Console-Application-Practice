845dc97b4cb85c78b75e1058bc13fafe8b6374c5
Nik Nadzirie Nik Kamaruzamil <nik.nadzirie@stratusauto.com>
2024-08-01 12:02:43 +0800

    Create To-Do app main menu list

--- Console Application/Program.cs
+++ Console Application/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Creating To Do Application on Console
+
+// Welcome message
+Console.WriteLine("Welcome To To-Do Application!\n");
+
+// Main menu
+Console.WriteLine("Main Menu: \n");
+Console.WriteLine("1. View Existed To-Do List");
+Console.WriteLine("2. Add New To-Do List");
+Console.WriteLine("3. Update To-Do List");
+Console.WriteLine("4. Delete To-Do List\n");
+
+// Wait for user to select the menu
+Console.Write("Enter Menu Number: ");
+Console.ReadLine();
