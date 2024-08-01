d3e7a05fc822f492a83ccdee5186351cd23cbffa
Nik Nadzirie Nik Kamaruzamil <nik.nadzirie@stratusauto.com>
2024-08-01 11:13:21 +0800

    hello world lesson

--- Console Application/Program.cs
+++ Console Application/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello World!");
+
+/*Console.Write("Hello ");
+Console.Write("World");*/
+
+//CODECADEMY CREATING A CREATURES
+
+// Console.WriteLine(" .-.");
+// Console.WriteLine("(o o)");
+// Console.WriteLine("| O |");
+// Console.WriteLine("|   |");
+// Console.WriteLine("'~~~'");
+
+// Console.WriteLine("Ghosty");
+
+Console.WriteLine(" c_     ___");
+Console.WriteLine("  \\\\\\\\__|__|__");
+Console.WriteLine("　　   \\\\( o_o)");
+Console.WriteLine("       > ~  >");
+Console.WriteLine("　　   　/ 　 / \\\\\\\\");
+
+Console.WriteLine("  　　 /　 　/　 \\\\\\\\");
+Console.WriteLine("　　   \\\\　 )　　c/");
+Console.WriteLine("　  　/ 　/");
+Console.WriteLine("　 /　 /|");
+Console.WriteLine("　(　( \\ ");
+
+Console.WriteLine("　|　|  \\ ");
+Console.WriteLine("　| / \\ )");
+Console.WriteLine("　| |　) |");
+Console.WriteLine(" /  )(_/");
+Console.WriteLine("(_ /");
