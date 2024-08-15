c8bb641b35f74171516322677e34915f718d2312
Nik Nadzirie Nik Kamaruzamil <nik.nadzirie@stratusauto.com>
2024-08-15 14:09:55 +0800

    add while loop to run continuosly

--- Console Application/Program.cs
+++ Console Application/Program.cs
@@ -5,28 +5,30 @@
 // r = Annual interest rate (decimal)
 // n = Number of times interest is compounded per year
 // t = Time in years
+while (true)
+{
+    Console.WriteLine("*****Welcome To Compound Interest Calculator*****\n");
 
-Console.WriteLine("*****Welcome To Compound Interest Calculator*****\n");
+    // P = Principal amount
+    Console.Write("Enter your amount of investment: ");
+    double P = Convert.ToDouble(Console.ReadLine());
 
-// P = Principal amount
-Console.Write("Enter your amount of investment: ");
-double P = Convert.ToDouble(Console.ReadLine());
+    // r = Annual interest rate (decimal)
+    Console.Write("\n\nEnter annual interest rate percentage (%): ");
+    double r = Convert.ToDouble(Console.ReadLine());
+    r = r / 100;
 
-// r = Annual interest rate (decimal)
-Console.Write("\n\nEnter annual interest rate percentage (%): ");
-double r = Convert.ToDouble(Console.ReadLine());
-r = r / 100;
+    // n = Number of times interest is compounded per year
+    Console.Write("\n\nEnter number of times interest compounded in a year: ");
+    int n = Convert.ToInt32(Console.ReadLine());
 
-// n = Number of times interest is compounded per year
-Console.Write("\n\nEnter number of times interest compounded in a year: ");
-int n = Convert.ToInt32(Console.ReadLine());
+    // t = Time in years
+    Console.Write("\n\nIn how many years you want to see you future value of invesment?: ");
+    int t = Convert.ToInt32(Console.ReadLine());
 
-// t = Time in years
-Console.Write("\n\nIn how many years you want to see you future value of invesment?: ");
-int t = Convert.ToInt32(Console.ReadLine());
+    // A = Future value of the investment
+    double A = Math.Round(P * Math.Pow((1 + (r / n)), (n * t)), 2, MidpointRounding.AwayFromZero);
 
-// A = Future value of the investment
-double A = Math.Round(P * Math.Pow((1 + (r/n)), (n*t)), 2, MidpointRounding.AwayFromZero);
-
-Console.WriteLine($"\n\nYour future value of the investment will be: {A}");
+    Console.WriteLine($"\n\nYour future value of the investment will be: {A}\n\n");
+}
 
